dbe0154097b12c1d72f76f74ae5b22171cc7195c
Trufaldino <anton.ryadinski@gmail.com>
2023-02-19 20:22:14 +0000

    added secret key

--- backend/Foodgram/Foodgram/settings.py
+++ backend/Foodgram/Foodgram/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1fv102$_^jkw-^zc5n6&4vg!4571kb#rt&2!8i0+wc-k%e-tr!'
+SECRET_KEY = os.getenv('SECRET_KEY', default='1fv102$_^jkw-^zc5n6&4vg!4571kb#rt&2!8i0+wc-k%e-tr!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
